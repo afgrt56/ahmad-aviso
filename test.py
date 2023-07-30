cbd59524ec99f13271273dd694458fb4dd48f9e6
afgrt56 <140928979+afgrt56@users.noreply.github.com>
2023-07-30 07:37:01 +0500

    Update test.py

--- test.py
+++ test.py
@@ -8,9 +8,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
-import chromedriver_autoinstaller
 import json
-chromedriver_autoinstaller.install()
+
 
 def running():
     
