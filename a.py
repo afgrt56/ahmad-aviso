ee5a1d97da866dc9b4faf10e331331561776e771
afgrt56 <140928979+afgrt56@users.noreply.github.com>
2023-07-31 17:19:56 +0500

    Update a.py

--- a.py
+++ a.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 options = webdriver.ChromeOptions()
 options.add_argument('--headless')  # To run Chrome in headless mode (without GUI)
 options.add_argument('--no-sandbox')  # Required for running as a superuser
-driver = webdriver.Chrome(executable_path='/usr/local/bin/chromedriver', options=options)
+driver = webdriver.Chrome(options=options)
 
 driver.get('https://www.google.com')
 search_box = driver.find_element_by_name('q')
